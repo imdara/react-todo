d648ea9ae95210c0a9d3c145bead6207c938e463
imdara <mayurdarade99gmail.com>
2024-07-20 22:52:59 +0530

    final commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,17 +4,30 @@ import Todos from "./Todos";
 
 const Home = () => {
   const [todo, setTodo] = useState(null);
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    let localItems = localStorage.getItem("ITEMS");
+    return localItems ? JSON.parse(localItems) : [];
+  });
+
+  useEffect(
+    () => localStorage.setItem("ITEMS", JSON.stringify(todos)),
+    [todos]
+  );
+
   const addTodo = () => {
     setTodos([...todos, todo]);
     setTodo(null);
   };
+
   const toggleCheckTodo = (srNo, checked) => {
-    setTodos([
-      ...todos.filter((todo) => todo.srNo != srNo),
-      { ...todos.find((todo) => todo.srNo === srNo), checked },
-    ]);
+    setTodos(
+      [
+        ...todos.filter((todo) => todo.srNo != srNo),
+        { ...todos.find((todo) => todo.srNo === srNo), checked },
+      ].sort((a, b) => a.srNo - b.srNo)
+    );
   };
+
   return (
     <>
       <Header />
